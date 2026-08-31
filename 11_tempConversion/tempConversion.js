77fde8ce9e116b067102fdd66ff537b1017140c8
GitMMara <240789219+GitMMara@users.noreply.github.com>
2026-08-31 11:18:47 +0300

    Exercises done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+const convertedC = (tempF - 32) * (5/9)
+return Math.round(convertedC * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+var convertedF = tempC * (9/5) + 32
+return Math.round(convertedF * 10)/10
+
 };
 
 // Do not edit below this line
